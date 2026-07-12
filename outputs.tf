39c5aadc71cce2f04176c706062cbf5b3210b25e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:05:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "spring_cloud_active_deployments_id" {
+  description = "Map of id values across all spring_cloud_active_deployments, keyed the same as var.spring_cloud_active_deployments"
+  value       = { for k, v in azurerm_spring_cloud_active_deployment.spring_cloud_active_deployments : k => v.id }
+}
 output "spring_cloud_active_deployments_deployment_name" {
   description = "Map of deployment_name values across all spring_cloud_active_deployments, keyed the same as var.spring_cloud_active_deployments"
   value       = { for k, v in azurerm_spring_cloud_active_deployment.spring_cloud_active_deployments : k => v.deployment_name }
